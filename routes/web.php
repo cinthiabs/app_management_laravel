421290753fccc010058a808f73a5592ca47531e9
cinthiabs <cinthiabarbosa8d@outlook.com>
2025-02-11 21:08:32 -0300

    parametros

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,6 @@ Route::get('/sobrenos', [SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [ContatoController::class, 'contato']);
 
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string $mensagem){    
+    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+});
