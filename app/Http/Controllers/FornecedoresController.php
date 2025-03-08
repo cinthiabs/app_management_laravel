966af3a89e5cc23f576c5711af4a2018dd493f04
cinthiabs <cinthiabarbosa8d@outlook.com>
2025-03-08 18:29:03 -0300

    add empty

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class FornecedoresController extends Controller
 {
     public function index() {
-        $fornecedores = [0 =>['nome' => 'Fornecedor 1', 'status' => 'S']];
+        $fornecedores = [
+            0 =>[
+                'nome' => 'Fornecedor 1', 
+                'status' => 'S',
+                'cnpj' => ''
+                ]
+            ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -11,23 +11,35 @@ comentarios de bloco
 
 @endphp
 
-{{-- usando if e else no blade --}}
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem muitos fornecedores cadastrados</h3>
-@else
-    <h3>Não existem fornecedores cadastrados</h3>
-@endif
+{{-- usando o Isset no blade  para validar se a variavel fornecedores existe --}}
+@isset($fornecedores)
+
+    {{-- usando if e else no blade --}}
+    @if(count($fornecedores) > 0 && count($fornecedores) < 10)
+        <h3>Existem alguns fornecedores cadastrados</h3>
+    @elseif(count($fornecedores) > 10)
+        <h3>Existem muitos fornecedores cadastrados</h3>
+    @else
+        <h3>Não existem fornecedores cadastrados</h3>
+    @endif
 
 
-Fornecedor: {{ $fornecedores[0]['nome'] }} <br>
-Status: {{ $fornecedores[0]['status'] }} <br>
+    Fornecedor: {{ $fornecedores[0]['nome'] }} <br>
+    Status: {{ $fornecedores[0]['status'] }} <br>
 
-{{-- usando o unless no blade --}}
-{{-- @if é o contrário do @unless --}}
-@unless($fornecedores[0]['status'] == 'S')
-    Fornecedor Inativo
-@endunless
+    {{-- usando o empty no blade --}}
+    {{-- @empty é o contrário do @isset  valida se existe conteudo armazenado --}}
+    @empty($fornecedores[0]['cnpj'])
+        CNPJ - Vazio
+    @endempty
+    
+    {{-- usando o unless no blade --}}
+    {{-- @if é o contrário do @unless --}}
+    @unless($fornecedores[0]['status'] == 'S')
+        Fornecedor Inativo
+    @endunless
 
-{{-- usando isset no blade --}}
+@endisset
+
+
+
